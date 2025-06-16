c1cfe43d0a85ec410b2d78af2bc52cb537fcccda
Isaac Pascoe <IsaacMPascoe@gmail.com>
2025-06-16 21:48:13 +0800

    refactor input handling and screen refresh logic in main function

--- src/main.zig
+++ src/main.zig
@@ -1,11 +1,16 @@
 const std = @import("std");
 
+
 const print = std.debug.print;
 
+pub fn ctrlKey(comptime key: u8) u8 {
+    return key & 0x1f;
+}
+
 /// Enables Raw Mode Input to terminal
-pub fn buffer_off(stdin: std.fs.File) !void {
+pub fn bufferOff(stdin: std.fs.File) !void {
     var terminal = std.posix.tcgetattr(stdin.handle) catch |err| {
-        die(err);
+        die(stdin, err);
     };
     terminal.cflag.CSIZE = .CS8;
     terminal.iflag = .{
@@ -24,14 +29,14 @@ pub fn buffer_off(stdin: std.fs.File) !void {
     terminal.oflag.OPOST = false;
 
     std.posix.tcsetattr(stdin.handle, .NOW, terminal) catch |err| {
-        die(err);
+        die(stdin, err);
     };
 }
 
 /// Disables Raw Mode Input to terminal
-pub fn buffer_on(stdin: std.fs.File) !void {
+pub fn bufferOn(stdin: std.fs.File) !void {
     var terminal = std.posix.tcgetattr(stdin.handle) catch |err| {
-        die(err);
+        die(stdin, err);
     };
     terminal.cflag.CSIZE = .CS5;
     terminal.iflag = .{
@@ -49,31 +54,60 @@ pub fn buffer_on(stdin: std.fs.File) !void {
     };
     terminal.oflag.OPOST = true;
     std.posix.tcsetattr(stdin.handle, .NOW, terminal) catch |err| {
-        die(err);
+        die(stdin, err);
     };
 }
 
 /// Prints error and exits progam
-pub fn die(message: anyerror) noreturn {
+pub fn die(stdin: std.fs.File, message: anyerror) noreturn {
+    refreshScreen(stdin) catch {};
     std.log.err("{any}", .{message});
     std.process.exit(1);
 }
 
-pub fn main() !void {
-    const stdin = std.io.getStdIn();
-    try buffer_off(stdin);
-    defer buffer_on(stdin) catch {};
+pub fn readKey(stdin: std.fs.File) !u8 {
+    return try stdin.reader().readByte();
+}
 
-    while (true) {
-        const char = try stdin.reader().readByte();
-        if (char == 'q') {
-            break;
-        }
-        if (std.ascii.isControl(char)) {
-            print("{d}\r\n", .{char});
-        } else {
-            print("{c}\r\n", .{char});
+pub fn drawRows() !void {
+    const rows = 24;
+    const stdout = std.io.getStdOut();
+
+    var i: u16 = 0;
+    while (i < rows) : (i += 1) {
+        _ = try std.posix.write(stdout.handle, "~");
+        if (i < rows - 1) {
+            _ = try std.posix.write(stdout.handle, "\r\n");
         }
     }
-    try buffer_on(stdin);
+}
+
+pub fn processKeypress(stdin: std.fs.File) !void {
+    const char = try readKey(stdin);
+    switch (char) {
+        ctrlKey('q') => {
+            std.process.exit(0);
+        },
+        else => {
+        },
+    }
+}
+
+pub fn refreshScreen(stdin: std.fs.File) !void {
+    _ = try std.posix.write(stdin.handle, "\x1b[2J");
+    _ = try std.posix.write(stdin.handle, "\x1b[H");
+    try drawRows();
+    _ = try std.posix.write(stdin.handle, "\x1b[H");
+}
+
+pub fn main() !void {
+    const stdin = std.io.getStdIn();
+    try bufferOff(stdin);
+    defer bufferOn(stdin) catch {};
+
+    while (true) {
+        try refreshScreen(stdin);
+        try processKeypress(stdin);
+    }
+    try bufferOn(stdin);
 }
